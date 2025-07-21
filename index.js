76f8967ce8b68d3b814ab465ce8f80ede5ecf8b8
shagormukthakim <shagormukthakim2143@gmail.com>
2025-07-21 10:30:34 +0600

    done button is now here

--- index.js
+++ index.js
@@ -10,13 +10,26 @@ btn.addEventListener("click",()=>{
 
     const div=document.createElement('div');
     div.innerHTML=`
-    <ul>
+    <ul class="ul">
     <li class="item">${e}</li>
+    <button class="b" onClick="deletes(div)">Done</button>
     </ul>
 
     
-    `
+    `;
+     const doneBtn = div.querySelector(".b");
+    doneBtn.addEventListener("click", () => {
+        lists.removeChild(div);
+    });
+    
     lists.appendChild(div);
     into.value="";
 
 })
+
+// function deletes(e){
+//     const lists=document.getElementById('list');
+//     lists.removeChild(e);
+
+
+// }
